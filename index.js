4d42a0336f6bdb62510af06c792bd0230e88374c
Ben Spotswood <spotswoodb@gmail.com>
2021-07-06 18:11:26 -0500

    rails s will not start the server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // any global variables
 const base_url = "http://127.0.0.1:3000"
-const contactService = new ContactService(base_url)
+const workoutService = new WorkoutService(base_url)
 // any initializations of the application
